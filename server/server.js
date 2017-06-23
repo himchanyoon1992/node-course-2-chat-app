8702573c183be58804294358ea17eb9da120912f
HimchanYoon19921203 <chan@30100inc.com>
2017-06-23 16:52:05 +0900

    Add newMessage

--- server/server.js
+++ server/server.js
@@ -14,6 +14,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createAt : 12333
+  });
+
+  socket.emit('newEmail', {
+
+    from: 'chan@example.com',
+    text: 'Hey. What is going on',
+    createAt: 123
+
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
