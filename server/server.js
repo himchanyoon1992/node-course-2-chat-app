46832a1e6177d7ffcd7db55af42effe3cb787146
HimchanYoon19921203 <chan@30100inc.com>
2017-06-23 20:02:04 +0900

    user

--- server/server.js
+++ server/server.js
@@ -14,14 +14,34 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit form Admin text Welcome to the chat app
+  socket.emit('newMessage', {
+      from: 'Amdin',
+      text: 'Welcome to the chat app',
+      createAt: new Date().getTime()
+  });
+
+  // socket.boradcast.emit from Admin text New user joined
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Amdin',
+    text: 'New user joined',
+    createAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from : message.from,
+    //   text : message.text,
+    //   createAt : new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
       from : message.from,
       text : message.text,
       createAt : new Date().getTime()
     });
+
   });
 
   socket.on('createEmail', (newEmail) => {
